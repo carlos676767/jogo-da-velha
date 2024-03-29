803e4de5544e19fc940035ae0adb0a5f8af1c223
carlos676767 <carlitobr730@gmail.com>
2024-03-29 10:27:00 -0700

    parte de colocar o x aparece

--- jogo/src/main.ts
+++ jogo/src/main.ts
@@ -1,1 +1,12 @@
 
+const cell = document.querySelectorAll(".cell") 
+const addXNaTabela = () => {
+  const marcar = "X"
+  cell.forEach(element => {
+    element.addEventListener("click", () => {
+      element.innerHTML = marcar
+    })
+  });
+}
+
+addXNaTabela()
